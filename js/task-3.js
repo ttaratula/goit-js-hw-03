21e1f88aa10ff9ccf2c857259eb796ed64dd665a
Tetiana Taratula <ttaratula@gmail.com>
2025-01-21 19:57:26 +0000

    hw-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,22 @@
-function checkForSpam(message) {
-    const lowerCaseMessage = message.toLowerCase();
-  
-    if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
-      return true; 
-    } else {
-      return false; 
+function filterArray(numbers, value){
+
+  const emptyArray =[];
+
+  for (let number of numbers) {
+    if (number > value) {
+      emptyArray.push(number);
     }
   }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+  return emptyArray;
+
+
+}
+
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
